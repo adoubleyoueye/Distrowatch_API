d62dea0d9e87cc0862e7bf405a2c69583588c537
adoubleyoueye <awi.mphaphuli@tuta.io>
2021-08-31 13:54:01 +0200

    update openapi info

--- distrowatch_api/urls.py
+++ distrowatch_api/urls.py
@@ -21,9 +21,9 @@ router.register(r'architecture', ArchitectureViewSet, 'architecture')
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="distrowatch_api",
-        default_version='v1',
-        description="API description",
+        title="Unofficial Distrowatch JSON API",
+        default_version='v1.2',
+        description="Distrowatch is JSON API which provides general information about various Linux distributions as well as other free software/open source Unix-like operating systems. Data sourced from https://distrowatch.com/",
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
@@ -34,5 +34,4 @@ urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$',
             schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
